fb7a903c7ef00bbe3c1acf0935228ae0c02e70ea
Travis Brady <travis.brady@gmail.com>
2014-03-27 13:41:57 -0500

    clean up Oliblinear.predict, remove old stuff and add two tests

--- lib/oliblinear.ml
+++ lib/oliblinear.ml
@@ -7,6 +7,7 @@ let (<-@) = Ctypes.(<-@)
 let (!@) = Ctypes.(!@)
 
 let save_model = L.save_model
+
 let load_model fn =
     match Sys.file_exists fn with
     | `Yes -> L.load_model fn
@@ -20,6 +21,14 @@ let destroy_param = L.destroy_param
 let check_parameter = L.check_parameter
 let check_probability_model = L.check_probability_model
 
+let get_bias m = Ctypes.getf (!@ m) L.bias
+
+let get_labels m =
+    let num_classes = get_nr_class m in
+    let _labels = Ctypes.allocate_n Ctypes.int ~count:num_classes in
+    let _ = Liblinear.get_labels m _labels in
+    Ctypes.Array.to_list (Ctypes.Array.from_ptr _labels num_classes)
+
 let make_feature_node idx value =
     let fn = Ctypes.make L.feature_node in
     let () = begin
@@ -28,50 +37,22 @@ let make_feature_node idx value =
     end in
     fn
 
-let feature_node_array lst feature_max =
+let feature_node_array lst feature_max ?(bias=(-1.0)) =
+    let bias_node = if bias >= 0.0 then
+        make_feature_node (feature_max + 1) bias
+    else
+        make_feature_node (-1) 0.0
+    in
     List.filter_map lst ~f:(fun (x, y) ->
         if x <= feature_max then Some (make_feature_node x y) else None
     )
-    |> (fun x -> List.concat [x; [make_feature_node (-1) 0.0; make_feature_node (-1) 0.0]])
+    |> (fun x -> List.concat [x; [make_feature_node (-1) 0.0; bias_node]])
     |> Ctypes.Array.of_list L.feature_node
     |> Ctypes.Array.start
 
-let bias_feature_node m =
-    let nrf = get_nr_feature m in
-    let bias = Ctypes.getf (!@ m) L.bias in
-    let bias_term =
-        if bias >= 0.0 then
-            make_feature_node (nrf + 1) bias
-        else
-            make_feature_node (-1) bias
-    in
-    bias_term
-
-let predict m fns =
-    let len = Array.length fns in
-    let fnp = Ctypes.allocate_n L.feature_node ~count:(len + 1) in
-
-    let bias_term = bias_feature_node m in
-
-    for i = 0 to (len - 1) do
-        let (x, y) = Array.get fns i in
-        let ft = make_feature_node x y in
-        (fnp +@ i) <-@ ft
-    done;
-    (fnp +@ len) <-@ bias_term;
-    L.predict m fnp
-
-let predict_list m fns =
-    let nrf = get_nr_feature m in
-    let len = List.length fns in
-    let fnp = feature_node_array fns nrf in
-    let bias_term = bias_feature_node m in
-    (fnp +@ len) <-@ bias_term;
-    L.predict m fnp
-
-let get_labels m =
-    let num_classes = get_nr_class m in
-    let _labels = Ctypes.allocate_n Ctypes.int ~count:num_classes in
-    let _ = Liblinear.get_labels m _labels in
-    Ctypes.Array.to_list (Ctypes.Array.from_ptr _labels num_classes)
+let predict model features =
+    let bias = get_bias model in
+    let nrf = get_nr_feature model in
+    let feature_nodes = feature_node_array features nrf ~bias in
+    L.predict model feature_nodes
 
--- lib_test/test_oliblinear.ml
+++ lib_test/test_oliblinear.ml
@@ -19,6 +19,10 @@ let test_get_labels _ =
     let labels = List.sort compare (Oliblinear.get_labels m) in
     assert_equal [-1; 1] labels
 
+let test_get_bias _ =
+    let m = Oliblinear.load_model fn in
+    assert_equal (-1.0) (Oliblinear.get_bias m)
+
 (*
  * Verify that prediction using heart_scale.model
  * matches result returned by Python lib from the liblinear/python README
@@ -26,15 +30,15 @@ let test_get_labels _ =
  *)
 let test_predict _ =
     let m = Oliblinear.load_model fn in
-    let nrf = Oliblinear.get_nr_feature m in
-    let features = Oliblinear.feature_node_array [(1, 1.0); (3, 1.0)] nrf in
-    assert_equal 1.0 (Liblinear.predict m features)
+    let features = [(1, 1.0); (3, 1.0)] in
+    assert_equal 1.0 (Oliblinear.predict m features)
 
 let suite= "Test Oliblinear" >:::
     ["test_load" >:: test_load;
      "test_nr_features" >:: test_nr_features;
      "test_nr_classes" >:: test_nr_classes;
      "test_get_labels" >:: test_get_labels;
-    "test_predict"    >::  test_predict]
+     "test_get_bias"   >:: test_get_bias;
+     "test_predict"    >::  test_predict]
 
 let _ = run_test_tt_main suite
